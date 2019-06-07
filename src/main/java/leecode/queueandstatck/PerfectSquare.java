349f2107bdf5b2575e82918a0add6936976ef259
huangguojun <huangguojun@zto.cn>
2019-06-07 20:08:42 +0800

    广度优先算法解决完全平方数问题

--- src/main/java/leecode/queueandstatck/PerfectSquare.java
+++ src/main/java/leecode/queueandstatck/PerfectSquare.java
@@ -1,23 +1,63 @@
 package leecode.queueandstatck;
 
+import javafx.util.Pair;
+
+import java.util.LinkedList;
+
 /**
  * @Author Huang Guojun
  * @Date 2019/6/1
- * @Discription  完全平方数
+ * @Discription 完全平方数
  * 给定正整数 n，找到若干个完全平方数（比如 1, 4, 9, 16, ...）使得它们的和等于 n。你需要让组成和的完全平方数的个数最少
- *
+ * <p>
  * 输入: n = 12
  * 输出: 3
  * 解释: 12 = 4 + 4 + 4.
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * 输入: n = 13
  * 输出: 2
- * 解释: 13 = 4 + 9.
+ * 解释: 13 = 4 + 9.             广度有优先的思想  依次减去比n小的完全平方数  知道变为0
  */
 public class PerfectSquare {
     public static void main(String[] args) {
+        int i = numSquares(13);
+        System.out.println(i);
+    }
+
+
+    public static int numSquares(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        // 构建了一个链表
+        LinkedList<Pair<Integer, Integer>> queue = new LinkedList<>();
+        // 在尾部添加了一个 n ,0
+        queue.addLast(new Pair<>(n, 0));
+        // 是否已访问的记录
+        boolean[] visited = new boolean[n + 1];
+        visited[n] = true;
+
+        while (!queue.isEmpty()) {
+            Pair<Integer, Integer> front = queue.removeFirst();
+            Integer num = front.getKey();
+            Integer step = front.getValue();
+            if (num == 0) {
+                return step;
+            }
+            for (int i = 0; num - i * i >= 0; i++) {
+                int a = num - i * i;
+                if (!visited[a]) {
+                    if (a == 0) {
+                        return step + 1;
+                    }
+                    queue.addLast(new Pair<>(num - i * i, step + 1));
+                    visited[num - i * i] = true;
+                }
+            }
+        }
+        return 0;
 
     }
 }
