bb458b62454d4ba77568b2768f37d9b403518ea9
huangguojun <huangguojun@zto.cn>
2019-07-30 09:19:00 +0800

    订阅发布

--- src/main/java/designmodde/publishsubscribe/Test.java
+++ src/main/java/designmodde/publishsubscribe/Test.java
@@ -1,20 +1,18 @@
 package designmodde.publishsubscribe;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * @Author Huang Guojun
  * @Date 2019/5/25
  * @Discription
  */
+
 public class Test {
     public static void main(String[] args) {
-        Subject subscriptionSubject = new SubscriptionSubject();
-        // 创建微信用户
-        WeixinUser huangguojun = new WeixinUser("黄国君");
-        WeixinUser lishengzhong = new WeixinUser("李胜忠");
-        WeixinUser buzhidao = new WeixinUser("buzhidao");
-        subscriptionSubject.attach(huangguojun);
-        subscriptionSubject.attach(lishengzhong);
-        subscriptionSubject.attach(buzhidao);
-        subscriptionSubject.notify("今天下雨了");
+        ApplicationContext context = new ClassPathXmlApplicationContext();
+        WeatherPublisher weatherPublisher = (WeatherPublisher) context.getBean("WeatherPublisher");
+        weatherPublisher.pushListener("下雨");
     }
 }
